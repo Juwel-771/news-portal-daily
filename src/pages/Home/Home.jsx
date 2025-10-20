8d1acb513787b8b21b9140494d454f6dae9e7a0d
Juwel-771 <juwelah99@gmail.com>
2025-10-21 04:20:48 +0600

    left and right side nav

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Header from '../Shared/Header/Header';
+import Navbar from '../Shared/Navbar/Navbar';
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
+            <Navbar></Navbar>
             <h1 className='text-3xl font-poppins'>This is Home</h1>  
         </div>
     );
