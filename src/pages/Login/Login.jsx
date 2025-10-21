5b40681f2d71ed54981ff84b87f61051533435a8
Juwel-771 <juwelah99@gmail.com>
2025-10-21 15:53:18 +0600

    daisy ui login form created to auth folder

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import Navbar from '../Shared/Navbar/Navbar';
+import React from "react";
+import Navbar from "../Shared/Navbar/Navbar";
 
 const Login = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <h2>Login to you ID</h2>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div>
+        <h2 className="text-3xl my-10 text-center">Login to you ID</h2>
+        <form className="md:w-3/4 lg:w-1/2 mx-auto">
+          <fieldset className="fieldset">
+            <label className="label">Email</label>
+            <input 
+                type="email"
+                name="email" 
+                className="input" 
+                placeholder="Email" />
+            <label className="label">Password</label>
+            <input 
+                type="password"
+                name="password" 
+                className="input" 
+                placeholder="Password" />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+          </fieldset>
+          <button className="btn btn-secondary mt-4">Login</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
