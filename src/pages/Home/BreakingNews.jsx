79c5c6fc88b21939a02baa84d6de16f09fb096b3
Juwel-771 <juwelah99@gmail.com>
2025-10-21 06:13:39 +0600

    Navbar created with icon login

--- src/pages/Home/BreakingNews.jsx
+++ src/pages/Home/BreakingNews.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Marquee from 'react-fast-marquee';
+import { Link } from 'react-router';
 
 const BreakingNews = () => {
     return (
-        <div>
+        <div className='flex'>
             <button className='btn btn-secondary'>Breaking News</button>
+            <Marquee pauseOnHover={true} speed={80}>
+                <Link>I can be a React component, multiple React components, or just some text.......</Link>
+                <Link>I can be a React component, multiple React components, or just some text.....</Link>
+                <Link>I can be a React component, multiple React components, or just some text......</Link>
+            </Marquee>
         </div>
     );
 };
