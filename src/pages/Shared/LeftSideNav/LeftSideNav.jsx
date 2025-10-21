a812c58c300261357d9c9ebcf990ce649cdfd505
Juwel-771 <juwelah99@gmail.com>
2025-10-21 07:03:41 +0600

    leftNavbar category created

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router';
 
 const LeftSideNav = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(()=>{
+        fetch('categories.json')
+        .then(res=>res.json())
+        .then(data=>setCategories(data))
+    }, []);
+
     return (
-        <div>
-            <h2>Left side nav</h2>
+        <div className='space-y-6'>
+           <h2 className='text-2xl'>All Categories</h2>
+           {
+                categories.map(category => <Link
+                    className='block ml-4 text-xl font-semibold'
+                    key={category.id}
+                    to={'/category/${category.id}'}>
+                        {category.name}
+                    </Link>)
+           } 
         </div>
     );
 };
